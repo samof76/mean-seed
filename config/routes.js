65ea4535e23103937ba7c58554a23059fb78ac66
Neil Fenton <neilfenton@Neils-iMac.local>
2013-11-02 17:56:32 -0400

    first commit

---
+++ config/routes.js
@@ -0,0 +1,38 @@
+var async = require('async');
+
+module.exports = function(app, passport, auth) {
+    //User Routes
+    var users = require('../app/controllers/users');
+    app.get('/signin', users.signin);
+    app.get('/signup', users.signup);
+    app.get('/signout', users.signout);
+
+    //Setting up the users api
+    app.post('/users', users.create);
+
+    app.post('/users/session', passport.authenticate('local', {
+        failureRedirect: '/signin',
+        failureFlash: 'Invalid email or password.'
+    }), users.session);
+
+    app.get('/users/me', users.me);
+    app.get('/users/:userId', users.show);
+
+    //Finish with setting up the userId param
+    app.param('userId', users.user);
+
+    //Article Routes
+    var articles = require('../app/controllers/articles');
+    app.get('/articles', articles.all);
+    app.post('/articles', auth.requiresLogin, articles.create);
+    app.get('/articles/:articleId', articles.show);
+    app.put('/articles/:articleId', auth.requiresLogin, auth.article.hasAuthorization, articles.update);
+    app.del('/articles/:articleId', auth.requiresLogin, auth.article.hasAuthorization, articles.destroy);
+
+    //Finish with setting up the articleId param
+    app.param('articleId', articles.article);
+
+    //Home route
+    var index = require('../app/controllers/index');
+    app.get('/', index.render);
+};
